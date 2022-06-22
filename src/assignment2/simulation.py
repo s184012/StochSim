b7079185542957f1fc02e88ddce3b4785eb4b267
Simon Majgaard <simon.majgaard@gmail.com>
2022-06-22 10:28:18 +0200

    refactoring

--- src/assignment2/simulation.py
+++ src/assignment2/simulation.py
@@ -17,9 +17,16 @@ arr_Times = [14.5, 11.0, 8.0, 6.5, 5.0, 13.0]
 
 len_stay = [2.9, 4.0, 4.5, 1.4, 3.9, 2.2]
 
-urgency = [7, 5 , 2, 10, 5, 0]
+urgency = [7, 5, 2, 10, 5, 0]
 
 bed_capacity = [55, 40, 30, 20, 20, 0]
+
+class SimulationConfig:
+
+    def __init__(self, switch_probabilities, mean_arrival_times, mean_stay_time, urgency, bed_capacities):
+        pass
+
+
 
 class WardType(Enum):
     A = 0
@@ -30,7 +37,7 @@ class WardType(Enum):
     F = 5
 
 
-class PatientType(Enum):
+class PatientState(Enum):
     A = 0
     B = 1
     C = 2
@@ -38,35 +45,35 @@ class PatientType(Enum):
     E = 4
     F = 5
     CURED = 6
-    LOST = 7
+    REJECTED = 7
 
 
 @dataclass(order=True)
 class Patient:
-    type: PatientType=field(compare = False)
+    state: PatientState=field(compare = False)
     ward: WardType=field(compare = False, init=False)
     penalty: int=field(compare=False, init=False)
     arrival_time: float
     stay_time: dict=field(compare = False)
 
     def __post_init__(self):
-        self.penalty = urgency[self.type.value]
-        self.ward = list(WardType)[self.type.value]
+        self.penalty = urgency[self.state.value]
+        self.ward = WardType(self.state.value)
 
 
 
 @dataclass
 class Ward:
-    type: WardType
-    capacity: int= field(init=False)
+    ward_type: WardType
+    capacity: int = field(init=False)
     urgency: int = field(init=False)
     total_number_of_treated_patients: int = 0
     rejected_patients: int = 0
     patients: list = field(default_factory=list)
     
     def __post_init__(self):
-        self.capacity = bed_capacity[self.type.value]
-        self.urgency = urgency[self.type.value]
+        self.capacity = bed_capacity[self.ward_type.value]
+        self.urgency = urgency[self.ward_type.value]
 
     @property
     def number_of_current_patients(self):
@@ -84,12 +91,17 @@ class Ward:
         heapq.heappush(self.patients, (patient.arrival_time + patient.stay_time, patient))
         self.total_number_of_treated_patients += 1
 
+    def reject_patient(self, patient: Patient):
+        patient.state = PatientState.REJECTED
+        self.reject_patient += 1
+
+    
     def update_patients(self, curTime):
         if not self.patients:
             return
 
         while len(self.patients) != 0 and self.patients[0][0] <= curTime:
-            self.patients[0][1].type = PatientType.CURED
+            self.patients[0][1].type = PatientState.CURED
             heapq.heappop(self.patients)
 
 
@@ -115,15 +127,18 @@ class HospitalSimulation:
         self.bed_dist = bed_distribution
         self.total_penalty = 0
 
-    def ward_switch(self, patient: Patient, P=P):
+    def switch_ward(self, patient: Patient, P=P):
         self.total_penalty += patient.penalty
-        ward_type = np.random.choice(a = list(WardType), p = P[patient.type.value,:])
-        next_ward = self.wards[ward_type]
+        next_ward = self.choose_new_ward(patient, P)
         if next_ward.is_full:
-            patient.type = PatientType.LOST
-            next_ward.rejected_patients += 1
+            next_ward.reject_patient(patient)
+        else:
+            next_ward.add_patient(patient)
 
-    
+    def choose_new_ward(self, patient: Patient, P=P):
+        ward_type = np.random.choice(a = list(WardType), p = P[patient.state.value,:])
+        return self.wards[ward_type]
+
     def sim_arr(self,curTime, arr_Time):
         return curTime + self.arr_dist.rvs(scale=1/arr_Time)
 
@@ -133,16 +148,16 @@ class HospitalSimulation:
     def simulate_year(self, pType = 'nof', bed_distribution=None):
         if bed_distribution is not None:
             self.bed_dist = bed_distribution
-
+        self.total_penalty = 0
         patient_q = self.sim_patients(type= pType, curTime=0)
         heapq.heapify(patient_q)
         t = 0
         while t <= 365:
             patient = heapq.heappop(patient_q)
+            t = patient.arrival_time
             new_patient = self.sim_patients(type=patient.type, curTime=t)
 
             self.assign_patient_to_ward(patient, curTime=t)
-            t = patient.arrival_time
 
             self.update_patient_q(patient_q, new_patient)
 
@@ -150,13 +165,13 @@ class HospitalSimulation:
     def sim_patients(self, curTime, type = 'all'):
         if (type == 'all' or type == 'nof'):
             patients = []
-            for pType, arr_time, stay_time in zip(list(PatientType), arr_Times, len_stay):
-                if (pType is PatientType.F and type == 'nof'):
+            for pType, arr_time, stay_time in zip(list(PatientState), arr_Times, len_stay):
+                if (pType is PatientState.F and type == 'nof'):
                     break
-                patient = Patient(type=pType, arrival_time = self.sim_arr(curTime, arr_Time = arr_time), stay_time = self.sim_stay(stay=stay_time))
+                patient = Patient(state=pType, arrival_time = self.sim_arr(curTime, arr_Time = arr_time), stay_time = self.sim_stay(stay=stay_time))
                 patients.append(patient)
         else:
-            patients = Patient(type = type, arrival_time = self.sim_arr(curTime, arr_Time = arr_Times[type.value]), stay_time = self.sim_stay(stay=len_stay[type.value]))
+            patients = Patient(state = type, arrival_time = self.sim_arr(curTime, arr_Time = arr_Times[type.value]), stay_time = self.sim_stay(stay=len_stay[type.value]))
         return patients
 
 
@@ -172,6 +187,6 @@ class HospitalSimulation:
         ward = self.wards[patient.ward]
         ward.update_patients(curTime = curTime)
         if ward.is_full:
-            self.ward_switch(patient)
+            self.switch_ward(patient)
         else:
             ward.add_patient(patient)
